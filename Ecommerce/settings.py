231fdbe4778220162822cfa75860dd6e3778e66b
Suraj Mhatre <surajmcodes@gmail.com>
2021-05-05 23:34:00 +0530

    initail commit

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '@@#c$0h44d!=fx79=k!kp33)k6utfe5k9#e^2^wmri=6#192-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
---
+++ README.md
@@ -0,0 +1,5 @@
+Ecommerce website
+- The Website is built using Django
+- User can order items using Guest login
+
+![Ecommerce-website](/ecom.gif)
